73beba554c95ad3b1ba3f06abe692312ba7a8b74
ShashiHarshakumar <ShashiHarshakumar@LAPTOP-86DJQS01>
2021-06-24 19:26:43 +0530

    first project

---
+++ JavaAssignment1/src/ifStatement/Program1.java
@@ -0,0 +1,16 @@
+package ifStatement;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+	int i = 5;
+		if(i<=5) {
+			
+			System.out.println("It is exicuted");	
+			
+		}
+
+	}
+
+}
